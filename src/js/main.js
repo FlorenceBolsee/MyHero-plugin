112dc6e370b3abdf25ff5e4a77fce7f04b343255
Florence Bolsée <florence.bolsee@gmail.com>
2017-12-13 12:46:19 +0100

    js

--- src/js/main.js
+++ src/js/main.js
@@ -18,27 +18,28 @@ var gradientDirection = [
 ];
 
 var i = 0;
+var n = 0;
 
 $(".heroplugin").click(
   function(){
     var $this = $(this);
     if($this.hasClass("hero--position")){
-      if(i != 0){
-        $this.removeClass(position[i - 1]);
+      for(n = 0; n < position.length; n++){
+        $this.removeClass(position[n]);
       }
       $this.addClass(position[i]);
-      if(i > position.length){
+      if(i >= position.length - 1){
         i = 0;
       } else {
         i ++;
       }
     }
     if($this.hasClass("hero--gradient")){
-      if(i != 0){
-        $this.removeClass(gradientDirection[i - 1]);
+      for(n = 0; n < gradientDirection.length; n++){
+        $this.removeClass(gradientDirection[n]);
       }
       $this.addClass(gradientDirection[i]);
-      if(i > gradientDirection.length){
+      if(i >= gradientDirection.length - 1){
         i = 0;
       } else {
         i ++;
